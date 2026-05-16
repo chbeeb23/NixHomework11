4b0ab863bc5c715822e8499e0d20da563b5e5f4f
mperepelitsa23 <chbeeb23@gmail.com>
2026-05-16 23:59:56 +0300

    NixHomework11: use line trace instead of Perception system

--- Source/NixHomework11/NixAIController.cpp
+++ Source/NixHomework11/NixAIController.cpp
@@ -7,45 +7,6 @@
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 
-#define TARGET_ACTOR_KEY "TargetActor"
-
-ANixAIController::ANixAIController()
-{
-	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
-	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
-
-	SightConfig->SightRadius = 2000.f;
-	SightConfig->LoseSightRadius = 2500.f;
-	SightConfig->PeripheralVisionAngleDegrees = 90.f;
-
-	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
-	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
-	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
-
-	AIPerception->ConfigureSense(*SightConfig);
-	AIPerception->SetDominantSense(SightConfig->GetSenseImplementation());
-}
-
-void ANixAIController::BeginPlay()
-{
-	Super::BeginPlay();
-
-	AIPerception->OnTargetPerceptionUpdated.AddDynamic(this, &ANixAIController::OnTargetDetected
-	);
-}
-
-void ANixAIController::OnTargetDetected(AActor* Actor, FAIStimulus Stimulus)
-{
-	if (Stimulus.WasSuccessfullySensed())
-	{
-		GetBlackboardComponent()->SetValueAsObject(TARGET_ACTOR_KEY, Actor);
-	}
-	else
-	{
-		GetBlackboardComponent()->ClearValue(TARGET_ACTOR_KEY);
-	}
-}
-
 void ANixAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
