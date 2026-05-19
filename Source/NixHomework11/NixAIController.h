11adb13691b0c7e67e0057be2a87e7914b57dcb2
mperepelitsa23 <chbeeb23@gmail.com>
2026-05-19 14:39:31 +0300

    Revert "NixHomework11: use line trace instead of Perception system"
    This reverts commit 4b0ab863bc5c715822e8499e0d20da563b5e5f4f.

--- Source/NixHomework11/NixAIController.h
+++ Source/NixHomework11/NixAIController.h
@@ -18,10 +18,25 @@ class NIXHOMEWORK11_API ANixAIController : public AAIController
 {
 	GENERATED_BODY()
 
+
+public:
+	// Sets default values for this pawn's properties
+	ANixAIController();
+
 protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	TObjectPtr<UAIPerceptionComponent> AIPerception;
+
+	UPROPERTY()
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI")
 	TObjectPtr<UBehaviorTree> BehaviorTree;
+
+	UFUNCTION()
+	void OnTargetDetected(AActor* Actor, FAIStimulus Stimulus);
 
 public:
 	virtual void OnPossess(APawn* InPawn) override;
